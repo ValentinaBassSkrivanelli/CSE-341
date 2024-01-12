23e6793ba97ee203112b7f3c700f0e6b94e1be7f
ValentinaBassSkrivanelli <aldanavalentinabass@gmail.com>
2024-01-11 23:27:12 -0300

    new account

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ app.get('/', (req, res) => {
 });
 const port = 3000
 app.listen(process.env.PORT || port);
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+  
